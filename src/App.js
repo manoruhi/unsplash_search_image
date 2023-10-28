ff32636a769a8851e7dbe0dd68363904b1021ee4
Rohit Baghel <rohitbaghel16890@gmail.com>
2023-10-28 17:58:57 +0530

    Changed in css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import {useState, useEffect} from 'react';
+import { useState } from 'react';
 import { LoadImages, SearchImages } from './components/api'
 import './App.css';
-import Image from './components/image'
-
+import Image from './components/image';
 function App() {
-  const [query, setQuery] = useState()
-  const [searchQ, setSearch] = useState()
+  const [query, setQuery] = useState();
+  const [queryQ, setSearch] = useState();
   const data = LoadImages();
-  //console.log(query);
+  // console.log(data);
+  
+  
   const search = () => {
-    setSearch(query)
-   
+    console.log(query);
+    setSearch(query);
   }
-  const searchData = SearchImages(query);
-  console.log(searchData)
+  const searchData = SearchImages(queryQ);
+  console.log(searchData);
   return (
-    <div className="App">
-      <div>
-        <input type = "test" onChange={(event) => setQuery(event.target.value)}/>
-        <button onClick = {search}>Search</button>
+    <div className="App" style={{display:'flex', justifyContent: 'center', alignItems:'center', flexDirection:'column'}}>
+      <div class="input-group m-2" style={{width:100+"vh", display:'flex', justifyContent: 'center', alignItems:'center'}}>
+        <input type="search" style={{width:10+"vh"}} className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" onChange={(event) => setQuery(event.target.value)} />
+        <button type="button" className="btn btn-outline-primary" onClick={search}>search</button>
       </div>
-      <div className = "contain">
-      {searchQ ? searchData.map((img,key) => (
-        <Image src = {img.urls.thumb} key = {key}/>
-    )) : data.map((img, key) => (
-       <Image src = {img.urls.thumb} key = {key}/>
-    ))}
+      <div className='contain'>
+      {queryQ ? searchData.map((img,key) => (
+          <Image src={img.urls.thumb} key={key}></Image>
+        ))
+        :data.map((img,key) => (
+          <Image src={img.urls.thumb} key={key}></Image>
+        ))
+        }
       </div>
     </div>
   );
